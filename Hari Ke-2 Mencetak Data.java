bd11deb671ac15f286044a0707d4c015106d3604
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-10 20:19:13 +0800

    Hari Ke-2 Mencetak Data.java

--- Hari Ke-2 Mencetak Data.java
+++ Hari Ke-2 Mencetak Data.java
@@ -2,9 +2,9 @@ package mencetakdata;
 public class MencetakData {
     public static void main(String[] args) {
         // Menampilkan/Mencetak Data
-                System.out.println("Hello World");
-                System.out.println("I am learning Java");
-                System.out.println("it is awesome");                
+        System.out.println("Hello World");
+        System.out.println("I am learning Java");
+        System.out.println("it is awesome!");                
     }
     
 }
