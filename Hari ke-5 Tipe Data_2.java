89d023d9d6fc77be8256735f1534c1aa6453dd74
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-13 20:27:24 +0800

    Tipe Data_Angka Bilangan Berkoma

---
+++ Hari ke-5 Tipe Data_2.java
@@ -0,0 +1,18 @@
+package tipe.data2;
+
+public class TipeData2 {
+    public static void main(String[] args) {
+    /*
+      Tipe Data Angka Bilangan Berkoma
+       Floating Point           -> float    -> 32 bit
+       Double Floating Point    -> double   -> 54 bit
+    */
+
+    float berat = 12.5f;
+    double tinggi = 167.67;
+    
+    System.out.println("Berat = " + berat + " kg");
+    System.out.println("Tinggi = " + tinggi + " cm");
+
+    }   
+}
