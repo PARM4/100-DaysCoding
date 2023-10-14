c3ab0e6305c7991a0c3a0dd8eb49c997c944cacd
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-14 23:44:24 +0800

    Tipe Data_Karakter

---
+++ Hari ke-6 Tipe Data_Karakter.java
@@ -0,0 +1,19 @@
+package tipedata_karakter;
+
+public class TipeData_Karakter {
+    public static void main(String[] args) {
+    
+    /*
+      Tipe Data Karakter
+       Char     -> char     -> 1 Carakter
+       String   -> String   -> Kata dan Kalimat
+    */
+    
+    char gender = 'P';
+    String nama = "Parma";
+    
+        System.out.println("Nama            = "+nama);
+        System.out.println("Jenis Kelamin   = "+gender);
+    }
+    
+}
