87bbd66523b8d3b1ddfdec2a86b684db4a1deff7
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-16 00:20:16 +0800

    Variabel

--- Hari ke-7 Variabel.java
+++ Hari ke-7 Variabel.java
@@ -1,1 +1,19 @@
 
+package variabel;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama, alamat;
+        nama = "Parma";
+        alamat = "Ujubg Baru";
+        int umur = 19;
+        double tinggi = 157.25;
+        
+        System.out.println("Nama = "+nama);
+        System.out.println("Alamat = "+alamat);
+        System.out.println("Umur = "+umur+" Tahun");
+        System.out.println("Tinggi = "+tinggi+" cm");
+    }
+    
+}
