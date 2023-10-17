23fe2f03481ff63603fcd5bbc975393d52aaeb8e
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-17 19:01:34 +0800

    Belajar output Java

---
+++ Day9 output.java
@@ -0,0 +1,18 @@
+package output;
+
+public class output {
+    public static void main(String[] args) {
+    	
+    	/*
+    	*  Perbedaan print dan println
+    	*    print   = akan mencetak tanpa adanya enter atau penambahan baris baru pada kalimat berikutnya.
+    	*    println = akan mencetak dengan penambahan enter.
+    	*/
+    	
+        System.out.println("1. println ");
+        System.out.println("2. println ");
+    	
+        System.out.print("1. print ");
+        System.out.print("2. print");
+    }
+}
