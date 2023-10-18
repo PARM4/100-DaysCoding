d92d178992fe9577e976531b599139100c836a3d
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-18 21:10:18 +0800

    Operator Aritmatika_penjumlahan(+)

---
+++ Day10 Operator.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int a, b, c;
+		
+		a = 12;
+		b = 7;
+		c = a + b;
+		
+		System.out.println ("Nilai a = " + a);
+		System.out.println ("Nilai b = " + b);
+		System.out.println ("Maka hasil dari penjumlahan adalah = " + c);
+		
+	}
+}
