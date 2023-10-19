caf54fd12106ddaddffd61371cff3b7f8c1025f4
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-19 20:39:16 +0800

    Operator Aritmatika_pengurangan(-)

---
+++ Day-11 Operator.java
@@ -0,0 +1,13 @@
+package operator_2;
+
+public class Operator_2 {
+    public static void main(String[] args) {
+        
+        int x=12-9, y=10-4;
+        int z = x-y;
+       
+         
+        System.out.println(z-y);
+    }
+    
+}
