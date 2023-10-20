c30769c40b8543a3dd1545e11668e01b3520b810
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-20 21:24:42 +0800

    Operator Aritmatika_perkalian(*)

---
+++ Day12 Operator.java
@@ -0,0 +1,13 @@
+package operator3;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		double berat = 1.2;
+					 berat*=0.9;
+
+		
+		System.out.println (berat*berat);
+		
+	}
+}
