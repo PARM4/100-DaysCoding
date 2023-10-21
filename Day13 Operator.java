b73a8487750a17648ae28a1108f13b5fe6263e1a
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-21 23:03:20 +0800

    Operator Aritmatika_pembagian(/)

--- Day13 Operator.java
+++ Day13 Operator.java
@@ -1,24 +1,24 @@
 package operator;
 
 public class Main {
-	public static void main(String[] args) {
+     public static void main(String[] args) {
 		
-		    // Mendefinisikan dua angka yang akan dibagi
+	// Mendefinisikan dua angka yang akan dibagi
 
-	 	 byte angka1 = 9;
-		 float angka2 = 4.2f;
+      byte angka1 = 9;
+      float angka2 = 4.2f;
 
         // Melakukan operasi pembagian
 
-     byte hasilByte = (byte)(angka1 / angka2);
-	 	 float hasilFloat = (float)(angka1 / angka2);
-		 double hasilDouble = angka1 / angka2;
+      byte hasilByte = (byte)(angka1 / angka2);
+      float hasilFloat = (float)(angka1 / angka2);
+      double hasilDouble = angka1 / angka2;
         
-       // Menampilkan hasil pembagian
+        // Menampilkan hasil pembagian
 
-    System.out.println("Hasil pembagian (byte)   : " + hasilByte);
-    System.out.println("Hasil pembagian (float)  : " + hasilFloat);
-    System.out.println("Hasil pembagian (double) : " + hasilDouble);
+     System.out.println("Hasil pembagian (byte)   : " + hasilByte);
+     System.out.println("Hasil pembagian (float)  : " + hasilFloat);
+     System.out.println("Hasil pembagian (double) : " + hasilDouble);
 
     }
 }
