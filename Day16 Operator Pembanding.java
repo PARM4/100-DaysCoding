1b09a26a156e405409caa5aa2bf933f208f0e970
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-24 22:39:10 +0800

    Operator Pembanding

---
+++ Day16 Operator Pembanding.java
@@ -0,0 +1,39 @@
+public class OperatorPembanding {
+
+    public static void main(String[] args) {
+ 
+ /*
+     Operator Pembanding
+      > lebih besar
+      < lebih kecil
+      >= lebih besar sama dengan
+      <= lebih kecil sama dengan
+      == sama dengan
+      != tidak sama dengan 
+ */
+ 
+        int nilaiA = 7;
+        int nilaiB = 5;
+        boolean hasil;
+
+        hasil = nilaiA > nilaiB;
+        System.out.println(hasil);
+
+        hasil = nilaiA < nilaiB;
+        System.out.println(hasil);
+
+        hasil = nilaiA >= nilaiB;
+        System.out.println(hasil);
+
+        hasil = nilaiA <= nilaiB;
+        System.out.println(hasil);
+
+        hasil = nilaiA == nilaiB;
+        System.out.println(hasil);
+
+        hasil = nilaiA != nilaiB;
+        System.out.println(hasil);
+
+    }
+
+}
