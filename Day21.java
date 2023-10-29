9985ec32d557064e12435a81ad2806015b4b6a93
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-10-29 20:04:26 +0800

    Perulangan

---
+++ Day21.java
@@ -0,0 +1,13 @@
+package perulangan;
+public class Main {
+	public static void main(String[] args) {
+		for (int a = 1; a <= 10; a++){
+        if (a % 2 == 0) {
+        System.out.println(a+" bilangan genap");
+        }else{
+        System.out.println(a+" bilangan ganjil");
+        }
+  	}
+    
+	}
+}
