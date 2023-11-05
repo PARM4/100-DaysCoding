52943ac0796bb462d6516095c811a8430775d0a9
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-11-05 21:28:31 +0800

    Program Zodiak

--- Day28.java
+++ Day28.java
@@ -13,22 +13,35 @@ public class Main {
         
         String zodiak =" ";
         
-        if ((bulan == 3 && tanggal >=21) || (bulan == 4 && tanggal <=19 )) {
+        if (bulan == 3 && tanggal >=21 || bulan == 4 && tanggal <=19 ) {
            zodiak = "Aries";
-        } else if ((bulan == 4 && tanggal >=20) || (bulan == 5 && tanggal <=20)) {
+        } else if (bulan == 4 && tanggal >=20 || bulan == 5 && tanggal <=20) {
            zodiak = "Taurus";
-        } else if ((bulan == 5 && tanggal >=21) || (bulan == 6 && tanggal <=20)) {
+        } else if (bulan == 5 && tanggal >=21 || bulan == 6 && tanggal <=20) {
            zodiak = "Gemini";
-        } else if ((bulan == 6 && tanggal >=21) || (bulan == 7 && tanggal <=22)) {
+        } else if (bulan == 6 && tanggal >=21 || bulan == 7 && tanggal <=22) {
       	 zodiak = "Cancer";
-        } else if ((bulan == 7 && tanggal >=23) || (bulan == 8 && tanggal <=22)) {
+        } else if (bulan == 7 && tanggal >=23 || bulan == 8 && tanggal <=22) {
           zodiak = "Leo";
-        } else if ((bulan == 8 && tanggal >=23) || (bulan == 9 && tanggal <=22)) {
+        } else if (bulan == 8 && tanggal >=23 || bulan == 9 && tanggal <=22) {
         zodiak = "Virgo";
-        } else if ((bulan == 9 && tanggal >=23) || (bulan == 10 && tanggal <=22)) {
+        } else if (bulan == 9 && tanggal >=23 || bulan == 10 && tanggal <=22) {
         zodiak = "Libra";
-        } else if ((bulan == 10 && tanggal >=23)|| (bulan == 11 && tanggal <=21)) {
+        } else if (bulan == 10 && tanggal >=23 || bulan == 11 && tanggal <=21) {
          zodiak = "Scorpio";
-        } else if ((bulan == 11 && tanggal >=22)|| (bulan == 12 && tanggal <=21)) {
+        } else if (bulan == 11 && tanggal >=22 || bulan == 12 && tanggal <=21) {
         zodiak = "Sagittarius";
-        } else if ((bulan == 12 && tanggal >=22)|| (bulan 
+        } else if (bulan == 12 && tanggal >=22 || bulan == 12 && tanggal <=21) {
+        zodiak = "Sagittarius";
+        } else if (bulan == 12 && tanggal >=22 || bulan == 1 && tanggal <=19 ) {
+       zodiak = "capricorn";
+        } else if (bulan == 1 && tanggal >=20 || bulan == 2 && tanggal <=18 ) {
+        zodiak = "Aquarius";
+        } else if ( bulan == 2 && tanggal >=19 ||  bulan == 3 && tanggal <= 20) {
+         zodiak = "Pisces";
+        } 
+             
+        System.out.println("Anda mempunyai zodiak : " + zodiak);
+    }
+    
+}
