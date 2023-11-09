741671059d72f6e41bdec9933ba5517b211a3252
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-11-09 08:38:29 +0800

    Zodiak

--- Day28.java
+++ Day28.java
@@ -1,47 +1,42 @@
 package zodiak;
 import java.util.Scanner;
 public class Main {
+    
     public static void main(String[] args) {
-        Scanner hewan = new Scanner(System.in);
-        int tanggal, bulan;
         
-        System.out.print("Masukkan tanggal lahir anda(1-31): ");
-        tanggal = hewan.nextInt();
-        
-        System.out.print("Masukkan bulan lahir anda(1-12):");
-        bulan = hewan.nextInt();
-        
-        String zodiak =" ";
-        
-        if (bulan == 3 && tanggal >=21 || bulan == 4 && tanggal <=19 ) {
-           zodiak = "Aries";
-        } else if (bulan == 4 && tanggal >=20 || bulan == 5 && tanggal <=20) {
-           zodiak = "Taurus";
-        } else if (bulan == 5 && tanggal >=21 || bulan == 6 && tanggal <=20) {
-           zodiak = "Gemini";
-        } else if (bulan == 6 && tanggal >=21 || bulan == 7 && tanggal <=22) {
-      	 zodiak = "Cancer";
-        } else if (bulan == 7 && tanggal >=23 || bulan == 8 && tanggal <=22) {
-          zodiak = "Leo";
-        } else if (bulan == 8 && tanggal >=23 || bulan == 9 && tanggal <=22) {
-        zodiak = "Virgo";
-        } else if (bulan == 9 && tanggal >=23 || bulan == 10 && tanggal <=22) {
-        zodiak = "Libra";
-        } else if (bulan == 10 && tanggal >=23 || bulan == 11 && tanggal <=21) {
-         zodiak = "Scorpio";
-        } else if (bulan == 11 && tanggal >=22 || bulan == 12 && tanggal <=21) {
-        zodiak = "Sagittarius";
-        } else if (bulan == 12 && tanggal >=22 || bulan == 12 && tanggal <=21) {
-        zodiak = "Sagittarius";
-        } else if (bulan == 12 && tanggal >=22 || bulan == 1 && tanggal <=19 ) {
-       zodiak = "capricorn";
-        } else if (bulan == 1 && tanggal >=20 || bulan == 2 && tanggal <=18 ) {
-        zodiak = "Aquarius";
-        } else if ( bulan == 2 && tanggal >=19 ||  bulan == 3 && tanggal <= 20) {
-         zodiak = "Pisces";
-        } 
-             
-        System.out.println("Anda mempunyai zodiak : " + zodiak);
+        Scanner input = new Scanner(System.in);
+        System.out.println("~Saya Dapat Memberitahukan Zodiak yang anda Miliki~");
+        System.out.print("Masukkan Tanggal Lahir Anda (1-31) : ");
+        int tanggal = input.nextInt();
+        System.out.print("Masukkan Bulan Lahir Anda (1-12)   : ");
+        int bulan = input.nextInt();
+        String zodiac ="";
+        if((tanggal>=21 && tanggal<=31 && bulan==3)||(tanggal>=1 && tanggal<=19 && bulan==4)){
+            zodiac = "Aries";
+        }else if((tanggal>=20 && tanggal<=31 && bulan==4)||(tanggal>=1 && tanggal>=20 && bulan==5)){
+            zodiac = "Taurus";
+        }else if((tanggal>=21 && tanggal<=31 && bulan==5)||(tanggal>=1 && tanggal<=20 && bulan==6)){
+            zodiac = "Gemini";
+        }else if((tanggal>=21 && tanggal<=31 && bulan==6)||(tanggal>=1 && tanggal<=22 && bulan==7)){
+            zodiac = "Cancer";
+        }else if((tanggal>=23 && tanggal<=31 && bulan==7)||(tanggal>=1 && tanggal<=22 && bulan==8)){
+            zodiac = "Leo";
+        }else if((tanggal>=23 && tanggal<=31 && bulan==8)||(tanggal>=1 && tanggal<=22 && bulan==9)){
+            zodiac = "Virgo";
+        }else if((tanggal>=23 && tanggal<=31 && bulan==9)||(tanggal>=1 && tanggal<=22 && bulan==10)){
+            zodiac = "Libra";
+        }else if((tanggal>=23 && tanggal<=31 && bulan==10)||(tanggal>=1 && tanggal<=21 && bulan==11)){
+            zodiac = "Scorpio";
+        }else if((tanggal>=22 && tanggal<=31 && bulan==11)||(tanggal>=1 && tanggal<=21 && bulan==12)){
+            zodiac = "Sagittarius";
+        }else if((tanggal>=22 && tanggal<=31 && bulan==12)||(tanggal>=1 && tanggal<=19 && bulan==1)){
+            zodiac = "Capricorn";
+        }else if((tanggal>=20 && tanggal<=31 && bulan==1)||(tanggal>=1 && tanggal<=18 && bulan==2)){
+            zodiac = "Aquarius";
+        }else if((tanggal>=19 && tanggal<=31 && bulan==2)||(tanggal>=1 && tanggal<=1 && bulan==3)){
+            zodiac = "Pisces";
+        }
+        System.out.println("Zodiac Anda adalah : "+zodiac);
     }
     
 }
