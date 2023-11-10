a29cbc07ca0f3bd0a7ce29e3be8274662894de4e
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-11-10 22:14:41 +0800

    Pola 7

---
+++ Day33.java
@@ -0,0 +1,17 @@
+package day33;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        for(int i = 1; i <= 7; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 7; j >= i; j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
