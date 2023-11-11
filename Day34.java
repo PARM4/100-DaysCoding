5bdd21c9499159cb840300731d8a40738a0e8630
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-11-11 20:43:07 +0800

    Pola 8 - 9

--- Day34.java
+++ Day34.java
@@ -1,16 +1,35 @@
-package day33;
+package day34;
 
 public class Main {
     
     public static void main(String[] args) {
-        for(int i = 1; i <= 7; i++){
+        System.out.println("---------------");
+        String nama8 = "~Pola 8~";
+        System.out.println(nama8);
+        System.out.println("---------------");
+        for(int i =1; i <= 4; i++){
+            System.out.println("");
+            for(int j = 4; j >= i; j--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+        }
+        System.out.println("");
+        System.out.println("---------------");
+        String nama9 = "~Pola 9~";
+        System.out.println(nama9);
+        System.out.println("---------------");
+        for(int i = 1; i <= 5; i++){
+            System.out.println("");
             for(int j = 1; j <= i; j++){
                 System.out.print(" ");
             }
-            for(int j = 7; j >= i; j--){
-                System.out.print("*");
+            for(int j = 5; j >= i; j--){
+                System.out.print("* ");
             }
-            System.out.println("");
+            
         }
     }
     
