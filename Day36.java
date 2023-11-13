d64782bb27a6d3b1e56206e61f716a54a04b2527
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-11-13 15:45:03 +0800

    Pola 12 - 13

---
+++ Day36.java
@@ -0,0 +1,50 @@
+package day36;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("---------------");
+        System.out.println("~Pola 12~");
+        System.out.println("---------------");
+        for(int i = 1; i < 5; i++){
+            for(int j = 5; j >= i; j--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 5; j >= i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("---------------");
+        System.out.println("~Pola 13~");
+        System.out.println("---------------");
+        for(int i = 1; i < 5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 5; j >= i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i <= 5; i++){
+            for(int j = 5; j >= i; j--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
