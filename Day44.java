15c04dfe7392b40a9256ad688c72deaf3faaa680
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-11-21 22:27:46 +0800

    Penggunaan Kopling pada Array

---
+++ Day44.java
@@ -0,0 +1,16 @@
+package day44;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Penggunaan Looping pada Array
+        
+        int[] a ={2,5,6,5,1,4,7,8};
+        int jml = a.length;
+        
+        for(int i = 0; i < jml; i++){
+            System.out.println(a[i]);
+        }
+    }
+    
+}
