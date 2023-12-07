3f9b96cc49747c2999d8a2d8b59ce9d2b9c5b31f
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-12-07 21:11:17 +0800

    Menampilkan array menggunakan for each

---
+++ Day60.java
@@ -0,0 +1,11 @@
+package coding;
+
+public class day60 {
+    public static void main(String[]args){
+        String[] nama ={"Parma","Azisa","Irna","Rindi","Evi"};
+        
+        for(String n : nama){
+            System.out.println(n);
+        }
+    }
+}
