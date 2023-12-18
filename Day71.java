6d560c129d0d95e9748e1a514ea3060658d55aa6
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-12-18 15:26:16 +0800

    equalsIgnoreCase

--- Day71.java
+++ Day71.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class day71 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        String nama = "Parma";
+        String nama = "parma";
         for(int i = 1; i <= 3; i++){
             System.out.print("masukkan sandi : ");
             String sandi = sc.nextLine();
             if(nama.equalsIgnoreCase(sandi)){
                 System.out.println("login sukses");
-                System.out.println();
-                System.out.println("~program selesai~");
                 break;
             }else{
                 System.out.println("gagal");
                 if(3 == i){
-                    System.out.println();
                     System.out.println("kesempatan habis");
                 }
             }
         }
+        System.out.println();
+        System.out.println("~program selesai~");
     }
 }
