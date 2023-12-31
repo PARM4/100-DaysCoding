eeb6af2b238d4eea9f98683a2cfe460425639d72
PARM4 <147224285+PARM4@users.noreply.github.com>
2023-12-31 18:18:07 +0800

    Pola 24

---
+++ Day84.java
@@ -0,0 +1,14 @@
+package coding;
+
+public class day84 {
+    public static void main(String[]args){
+        System.out.println("~Pola 24~");
+        int a = 5;
+        for(int i=1; i<=a; i++){
+            for(int j=1; j<=a; j++){
+                System.out.print("+ ");
+            }
+            System.out.println();
+        }
+    }
+}
