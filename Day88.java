caee0b6e2dfde75dcb04d86ca41626147643b2dc
PARM4 <147224285+PARM4@users.noreply.github.com>
2024-01-04 19:54:05 +0800

    Array 2D

---
+++ Day88.java
@@ -0,0 +1,12 @@
+package coding;
+
+public class day88 {
+    public static void main(String[]args){
+      String[][] buah ={ 
+          {"mangga","durian","anggur"},
+          {"apel","pepaya","semangka"},
+      };
+        System.out.println(buah[0][2]);
+        System.out.println(buah[1][2]);
+    }
+}
